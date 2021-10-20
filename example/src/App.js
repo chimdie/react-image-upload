c26744fc13aa117e9d0987637abd0827a5425e79
chimdi nze <nzechimdi@yahoo.com>
2021-10-20 11:53:47 +0100

    initials app

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,18 @@ import { ExampleComponent } from 'react-image-upload'
 import 'react-image-upload/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  function getImageFileObject(imageFile) {
+    console.log({ onAdd: imageFile })
+  }
+  function runAfterImageDelete(file) {
+    console.log({ onDele: file })
+  }
+  return (
+    <ExampleComponent
+      onFileAdded={(img) => getImageFileObject(img)}
+      onFileRemoved={(img) => runAfterImageDelete(img)}
+    />
+  )
 }
 
 export default App
