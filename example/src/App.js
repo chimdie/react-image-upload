7a90723fbd7686531b83a81f584de730b7ddf177
chimdi nze <nzechimdi@yahoo.com>
2021-10-21 10:58:20 +0100

    gives user access to change the upload icon

--- example/src/App.js
+++ example/src/App.js
@@ -18,11 +18,41 @@ const App = () => {
           onFileRemoved={(img) => runAfterImageDelete(img)}
         />
       </div>
-      <div className='m-4'>
+      <div className='m-4 imgWrapper'>
         <ImageUploader
           onFileAdded={(img) => getImageFileObject(img)}
           onFileRemoved={(img) => runAfterImageDelete(img)}
-          uploadElement={<div>upload button</div>}
+          uploadElement={
+            <div>
+              <svg
+                class='svg-circleplus'
+                viewBox='0 0 100 100'
+                style={{ height: '20px', stroke: 'violet' }}
+              >
+                <circle
+                  cx='50'
+                  cy='50'
+                  r='45'
+                  fill='none'
+                  stroke-width='7.5'
+                ></circle>
+                <line
+                  x1='32.5'
+                  y1='50'
+                  x2='67.5'
+                  y2='50'
+                  stroke-width='5'
+                ></line>
+                <line
+                  x1='50'
+                  y1='32.5'
+                  x2='50'
+                  y2='67.5'
+                  stroke-width='5'
+                ></line>
+              </svg>
+            </div>
+          }
         />
       </div>
     </div>
